e40d9af95f31c80e6b1124b46c69392f0ff77384
Mosuhli-Coder <drmosuhli@gmail.com>
2021-08-07 15:19:37 +0200

    Saturday Commit Success

---
+++ optional-extra-practice/src/ScoopOption.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import Col from 'react-bootstrap/Col';
+import From from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import './App.css';
+function ScoopOption({ name, imagePath, updateItemCount}) {
+
+  //const [order]
+
+  return (
+    <div className="ScoopOption">
+    </div>
+  ); 
+
+
+}
+
+export default ScoopOption;
